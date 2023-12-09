181fc5ab5894455bf5ffe73ed1ca941ac1f2d1e8
hirahul0011 <ra_10001@yahoo.co.in>
2023-12-09 19:30:09 +0530

    app.modules finalized

--- onlinetest_Angular/src/app/app.module.ts
+++ onlinetest_Angular/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OnlineTestComponent } from './components/online-test/online-test.component';
 import { OnlineTestResultComponent } from './components/online-test-result/online-test-result.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { OnlineTestResultComponent } from './components/online-test-result/onlin
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
