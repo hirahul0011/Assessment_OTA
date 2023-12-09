8f397cfc7f2c1cd1c02fa9496449c2d2b040feaa
hirahul0011 <ra_10001@yahoo.co.in>
2023-12-09 20:24:36 +0530

    test-paper-question.service finalized

--- onlinetest_Angular/src/app/services/test-paper-question.service.ts
+++ onlinetest_Angular/src/app/services/test-paper-question.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TestPaperQuestion } from '../models/test-paper-question.model';
+
+const baseUrl = 'http://localhost:8080/api/testPaperQuestions';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TestPaperQuestionService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAll(): Observable<TestPaperQuestion[]> {
+    return this.http.get<TestPaperQuestion[]>(baseUrl);
+  }
+
+  get(id: any): Observable<TestPaperQuestion> {
+    return this.http.get<TestPaperQuestion>(`${baseUrl}/${id}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+  deleteAll(): Observable<any> {
+    return this.http.delete(baseUrl);
+  }
+
 }
